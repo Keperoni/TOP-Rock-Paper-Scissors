852bc617f90a18e8f47356a262e12340516657c0
Keperoni <pedrognf@hotmail.com>
2023-02-23 21:23:44 +0000

    Add JS Script and create getComputerChoice function

---
+++ javascript.js
@@ -0,0 +1,25 @@
+function getComputerChoice() {
+    let computerChoice = Math.floor(Math.random() * 3);
+
+        if (computerSelection === 1) {
+            computerSelection = "Rock"
+        }
+        else if (computerSelection === 2) {
+            computerSelection = "Paper"
+        }
+        else {
+            computerSelection = "Scissors"
+        }
+        console.log(computerSelection)
+    return computerSelection;
+    }
+
+function playRound(computerSelection, playerSelection) {
+
+
+}
+
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!FAZER COMITS!!!!!!!!!!!!!!!!!!!!!!!!!1
+
+getComputerChoice()
